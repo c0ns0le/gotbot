eb0fcac3b1614bece6db8267a69ce067731c60b6
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-13 22:52:44 -0700

    crash

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());         
+app.use(bodyParser.urlencoded({ extended: true }));                                
+app.use(multer);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
