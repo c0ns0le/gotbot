1185280219b0521717b3f8d11e09a6b42aec1dd9
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-13 22:40:49 -0700

    register to receive message posts

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ app.get('/webhooks', function (request, response) {
   response.send('Error, wrong validation token');
 });
 
+app.post('/webhooks/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log('message-received: ' + text);
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
