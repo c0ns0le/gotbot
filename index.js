23e9a16a77ae8c4aa8632f5aa0ea1d354ee0537d
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-13 22:01:06 -0700

    gotbot

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -11,6 +12,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/cool', function (request, response) {
+    response.send(cool());
+});
+
+app.get('/gotbot', function (request, response) {
+    response.render('pages/gotbot');
 });
 
 app.listen(app.get('port'), function() {
