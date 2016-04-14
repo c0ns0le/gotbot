d40bac0be33fc8a3900ef6da6dbb4d0e32d10929
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-13 23:15:13 -0700

    enable message processing

--- index.js
+++ index.js
@@ -36,16 +36,16 @@ app.get('/webhooks', function (request, response) {
 app.post('/webhooks/', function (req, res) {
 
   console.log('incoming-request' + JSON.stringify(req.body));
-  // messaging_events = req.body.entry[0].messaging;
-  // for (i = 0; i < messaging_events.length; i++) {
-  //   event = req.body.entry[0].messaging[i];
-  //   sender = event.sender.id;
-  //   if (event.message && event.message.text) {
-  //     text = event.message.text;
-  //     // Handle a text message from this sender
-  //     console.log('message-received: ' + text);
-  //   }
-  // }
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log('message-received: ' + text);
+    }
+  }
   res.sendStatus(200);
 });
 
