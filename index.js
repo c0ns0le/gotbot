a036e37af3554943f405021de7ce7f09f5a87f21
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-13 22:58:19 -0700

    hack

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json());         
-app.use(bodyParser.urlencoded({ extended: true }));                                
-app.use(multer);
+// app.use(bodyParser.json());         
+// app.use(bodyParser.urlencoded({ extended: true }));                                
+// app.use(multer);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -38,16 +38,16 @@ app.get('/webhooks', function (request, response) {
 app.post('/webhooks/', function (req, res) {
 
   console.log('incoming-request' + JSON.stringify(req.body));
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      // Handle a text message from this sender
-      console.log('message-received: ' + text);
-    }
-  }
+  // messaging_events = req.body.entry[0].messaging;
+  // for (i = 0; i < messaging_events.length; i++) {
+  //   event = req.body.entry[0].messaging[i];
+  //   sender = event.sender.id;
+  //   if (event.message && event.message.text) {
+  //     text = event.message.text;
+  //     // Handle a text message from this sender
+  //     console.log('message-received: ' + text);
+  //   }
+  // }
   res.sendStatus(200);
 });
 
