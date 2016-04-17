5599601394d41a8d54fe7176f31f72d102672450
Gautam Swaminathan <gautam.swaminathan@hbo.com>
2016-04-17 11:15:24 -0700

    order number plus plus

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.get('/webhooks', function (request, response) {
 });
 
 var userMap = {};
+var orderNumber = 12345678903;
 
 app.post('/webhooks/', function (req, res) {
 
@@ -68,14 +69,14 @@ app.post('/webhooks/', function (req, res) {
       setTimeout(function () {
 
         console.log('after some time in sendReceiptMessage');
-        
+
         var messageData = {
           "attachment":{
             "type":"template",
             "payload":{
               "template_type":"receipt",
               "recipient_name":"Gautam Swaminathan",
-              "order_number":"12345678902",
+              "order_number":orderNumber.toString(),
               "currency":"USD",
               "payment_method":"Visa 2345", 
               "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
@@ -113,6 +114,8 @@ app.post('/webhooks/', function (req, res) {
             }
           }
         }
+
+        orderNumber = orderNumber+1;
 
         finalSendMessage(sender, messageData);
       }, 3000);
